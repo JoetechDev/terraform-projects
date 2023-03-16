c114607de044a80c056d81e50d00f12d613f8d6c
jeaggrey <jeaggrey@gmail.com>
2023-03-15 23:09:02 -0400

    Added cloud statestore

--- main.tf
+++ main.tf
@@ -1,10 +1,21 @@
 module "s3bucket" {
-  source = "./s3bucket"
+  source  = "./s3bucket"
+  version = "1.0.0"
 
 }
+
+terraform {
+  cloud {
+    organization = "Joetech"
+
+    workspaces {
+      name = "Joetech"
+    }
+  }
+}
 output "module_random" {
-    value = module.s3bucket.random_number
-  
+  value = module.s3bucket.random_number
+
 }
 
 output "aws_kms_key" {
